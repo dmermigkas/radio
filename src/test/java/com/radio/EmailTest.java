7de34fc8adf482ad1945184c5d936a26baca8ff6
achiskon <achiskon@gmail.com>
2017-02-23 00:44:32 +0200

    email test fix

--- src/test/java/com/radio/EmailTest.java
+++ src/test/java/com/radio/EmailTest.java
@@ -1,7 +1,17 @@
 package com.radio;
 
+import com.radio.Models.EmailAddress;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by agk on 22/02/2017.
  */
 public class EmailTest {
+
+    @Test
+    public void emailConstructor() {
+        EmailAddress emailAddress = new EmailAddress("testing@email.org");
+        Assert.assertEquals("testing@email.org", emailAddress.getEmail());
+    }
 }
