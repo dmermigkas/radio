95df85bd4ced0379bffeaef7147c97ea1042a68f
Dimitris Verdelis <jimverdelis@gmail.com>
2017-02-22 23:54:46 +0200

    .

--- src/test/java/com/radio/PlayBackZoneTest.java
+++ src/test/java/com/radio/PlayBackZoneTest.java
@@ -1,11 +1,19 @@
 package com.radio;
 
+import com.radio.Models.PlayBackZone;
 import org.junit.Before;
+
+import java.util.Calendar;
 
 /**
  * Created by lotv on 22/02/2017.
  */
 public class PlayBackZoneTest {
+
+    private PlayBackZone playBackZone;
+
     @Before
-    public void Pla
+    public void PlayBackZoneTestSetUp(){
+        playBackZone= new PlayBackZone(Calendar.getInstance(), Calendar.getInstance()); //stub calendar instances?
+    }
 }
