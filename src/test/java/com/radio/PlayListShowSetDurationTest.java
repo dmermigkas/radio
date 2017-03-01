c697313392c0d90461a8f13279c4be40864fdde4
Dimitris Verdelis <jimverdelis@gmail.com>
2017-03-01 21:16:44 +0200

    .

--- src/test/java/com/radio/PlayListShowSetDurationTest.java
+++ src/test/java/com/radio/PlayListShowSetDurationTest.java
@@ -1,5 +1,11 @@
 package com.radio;
 
+import com.radio.Controllers.PlaylistShowController;
+import com.radio.Models.MusicTrack;
+import com.radio.Models.PlaylistShow;
+import com.radio.Models.Show;
+import com.radio.Models.Track;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -8,7 +14,24 @@ import org.junit.Test;
 public class PlayListShowSetDurationTest {
 
     @Test
-    public void PlayListShowSetDurationTest(){
+    public void PlayListShowControllerTest(){
+
+        PlaylistShowController controller = new PlaylistShowController();
+        Track track = new MusicTrack();
+        track.setDuration(100);
+        PlaylistShow show = new PlaylistShow();
+        show.setDuration(99);
+        show.addTrackToList(track);
+        Assert.assertTrue(controller.checkDuration(show));
+        show.setDuration(101);
+        Assert.assertFalse(controller.checkDuration(show));
+
+        Track track1 = new MusicTrack();
+        track1.setDuration(2);
+        show.addTrackToList(track1);
+        Assert.assertTrue(controller.checkDuration(show));
+        controller.fixDuration(show);
+        Assert.assertFalse(controller.checkDuration(show));
 
     }
 }
