27bb3d4fa44423b2b4ae3a606509405f1c2088a4
Dimitris Verdelis <jimverdelis@gmail.com>
2017-03-11 20:33:23 +0200

    Added Music Track Resource class

---
+++ src/main/java/com/radio/Resources/MusicTrackInfo.java
@@ -0,0 +1,139 @@
+package com.radio.Resources;
+
+import com.radio.Factories.MusicTrackFactoryImpl;
+import com.radio.Models.Genre;
+import com.radio.Models.MusicTrack;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lotv on 11/03/2017.
+ */
+@XmlRootElement
+public class MusicTrackInfo {
+
+    private Integer id;
+    private String title;
+    private String artist;
+    private Integer duration;
+    private Integer releaseYear;
+    private String genre;
+
+    public MusicTrackInfo(String title, String artist, Integer duration, Integer releaseYear, String genre) {
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+    }
+
+    public MusicTrackInfo(Integer id, String title, String artist, Integer duration, Integer releaseYear, String genre) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration;
+        this.releaseYear = releaseYear;
+        this.genre = genre;
+    }
+
+    public MusicTrackInfo(MusicTrack musicTrack){
+        id=musicTrack.getId();
+        title=musicTrack.getTitle();
+        artist=musicTrack.getArtist();
+        duration=musicTrack.getDuration();
+        releaseYear=musicTrack.getReleaseYear();
+        genre=musicTrack.getGenre().getGenre();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Integer getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Integer releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public MusicTrackInfo wrap(MusicTrack musicTrack) {return new MusicTrackInfo(musicTrack)}
+
+
+    public static List<MusicTrackInfo> wrap(List<MusicTrack> musicTracks) {
+
+        List<MusicTrackInfo> musicTrackInfoList = new ArrayList<>();
+
+        for (MusicTrack b : musicTracks) {
+            musicTrackInfoList.add(new MusicTrackInfo(b));
+        }
+
+        return musicTrackInfoList;
+
+    }
+
+    public MusicTrack getMusicTrack(MusicTrackFactoryImpl musicTrackFactory){ //todo check if factory is used properly
+
+        MusicTrack musicTrack = null;
+
+        if (id != null) {
+            musicTrack = musicTrackFactory.get(id);
+        } else {
+            musicTrack = new MusicTrack();
+        }
+
+        musicTrack.setArtist(artist);
+        musicTrack.setTitle(title);
+        musicTrack.setDuration(duration);
+        musicTrack.setReleaseYear(releaseYear);
+
+
+        if (musicTrack.getGenre() == null || !musicTrack.getGenre().getGenre().equals(genre)) {
+            musicTrack.setGenre(new Genre(genre));
+        }
+
+        return musicTrack;
+    }
+
+
+
+
+}
