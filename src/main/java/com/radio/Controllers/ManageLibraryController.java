4062fd89fe767d6341601c7facd5a44dc8d337dd
Dimitris Verdelis <jimverdelis@gmail.com>
2017-03-12 17:05:14 +0200

    Rest for music track will be ready soon

--- src/main/java/com/radio/Controllers/ManageLibraryController.java
+++ src/main/java/com/radio/Controllers/ManageLibraryController.java
@@ -4,14 +4,13 @@ import com.radio.Factories.FactoryGenericImpl;
 import com.radio.Models.MusicTrack;
 import com.radio.Resources.MusicTrackInfo;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
+import java.util.List;
 
 /**
  * Created by lotv on 11/03/2017.
@@ -38,6 +37,30 @@ public class ManageLibraryController {
         musicTrackFactory.create(musicTrack);
 
         return Response.created(newMusicTrackUri).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<MusicTrackInfo> listAllMusicTracks() {
+        List<MusicTrack> musicTracks = musicTrackFactory.getAll();
+
+        List<MusicTrackInfo> musicTrackInfo = MusicTrackInfo.wrap(musicTracks);
+
+        return musicTrackInfo;
+
+    }
+
+    @GET
+    @Path("{musicTrackId:[0-9]*}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public MusicTrackInfo getMusicTrackById(@PathParam("musicTrackId") int trackId) {
+
+        MusicTrack musicTrack = (MusicTrack)musicTrackFactory.get(trackId);
+
+        MusicTrackInfo musicTrackInfo = MusicTrackInfo.wrap(musicTrack);
+
+        return musicTrackInfo;
+
     }
 
 
