c1ff3fc15232865089dbf093c093e5c5893c65f5
Dimitris Verdelis <jimverdelis@gmail.com>
2017-03-18 17:56:50 +0200

    .

--- src/test/java/REST/MusicTrackRESTTest.java
+++ src/test/java/REST/MusicTrackRESTTest.java
@@ -2,6 +2,7 @@ package REST;
 
 import com.radio.Models.Genre;
 import com.radio.Models.MusicTrack;
+import com.radio.Resources.MusicTrackInfo;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,7 +16,22 @@ public class MusicTrackRESTTest {
     @Before
     public void MusicTrackSetup(){
         musictrack = new MusicTrack("musictitle", "musicartist", new Genre("musicgenre"), 1999, 100);
+
     }
 
+
+    @Test
+    public void testListBookById() {
+
+        // get all books
+        List<MusicTrackInfo> books = target(BOOKS).request().get(new GenericType<List<BookInfo>>() {
+        });
+
+        String firstBookId = Integer.toString(books.get(0).getId());
+
+        BookInfo book = target(bookIdUri(firstBookId)).request().get(BookInfo.class);
+        Assert.assertNotNull(book);
+        Assert.assertEquals("The Unified Modeling Language User Guide", book.getTitle());
+    }
 
 }
