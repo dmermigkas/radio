28901d9964bebb2bc59bfcf65f847d48293eaba4
Dimitris Verdelis <jimverdelis@gmail.com>
2017-03-21 20:16:09 +0200

    Fixed GenreRestTest

--- src/test/java/REST/GenreRESTTest.java
+++ src/test/java/REST/GenreRESTTest.java
@@ -1,11 +1,7 @@
 package REST;
 
 import com.radio.Controllers.GenreLibraryController;
-import com.radio.Controllers.MusicTracksLibraryController;
-import com.radio.Initializer;
 import com.radio.Models.Genre;
-import com.radio.Models.MusicTrack;
-import com.radio.Resources.MusicTrackInfo;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.junit.Assert;
 import org.junit.Before;
@@ -33,25 +29,20 @@ public class GenreRESTTest extends RESTTest { //todo needs work
 
     @Before
     public void setUpData() {
-        Initializer dataHelper = new Initializer();
-        dataHelper.eraseData();
         genre1 = new Genre("rock");
-        genre2 = new Genre( "pop");
-        Response response = target("genreLibrary").request().post(Entity.json(genre1));
-        response =  target("genreLibrary").request().post(Entity.json(genre2));
+        genre2 = super.createGenre(genre1);
     }
 
 
     @Test
     public void testListAllGenres() {
         String response = target("genreLibrary").request().get(String.class);
-        System.out.println(response);
-        Assert.assertEquals("[{\"genre\":\"rock\"},{\"genre\":\"pop\"}]", response);
+        Assert.assertEquals("[{\"genre\":\"rock\"}]", response);
     }
 
     @Test
     public void testGetGenreById() {
-        String response = target("genreLibrary/"+genre1.getId()).request().get(String.class); //todo what is the id of a valid genre in the database? oeo?
+        String response = target("genreLibrary/"+genre1.getId()).request().get(String.class);
         Assert.assertTrue(response.contains("rock"));
     }
 
@@ -66,17 +57,17 @@ public class GenreRESTTest extends RESTTest { //todo needs work
 
     @Test
     public void testDeleteGenreById() {
-        Response response = target("genreLibrary/9").request().delete(); ////todo what is the id of a valid genre in the database? oeo?
-        String allTracks = target("genreLibrary").request().get(String.class);
-        Assert.assertFalse(allTracks.contains("genre"));
+        Response response = target("genreLibrary/"+genre2.getId()).request().delete();
+        String allGenres = target("genreLibrary").request().get(String.class);
+        Assert.assertFalse(allGenres.contains("genre"));
     }
 
     @Test
-    public void testUpdateGenre() {
-        Genre genre = new Genre("death metal");
-        Response response = target( "genreLibrary/9").request().put(Entity.entity(genre, MediaType.APPLICATION_JSON)); //todo I need to make sure the id is correct in the path
-        String allTracks = target("genreLibrary").request().get(String.class);
-        Assert.assertFalse(allTracks.contains("death metal"));
+    public void testUpdateGenre() {//todo does not want to run
+        genre2.setGenre("death metal");
+        Response response = target( "genreLibrary/"+genre2.getId()).request().put(Entity.entity(genre2, MediaType.APPLICATION_JSON)); //todo I need to make sure the id is correct in the path
+        String allGenres = target("genreLibrary").request().get(String.class);
+        Assert.assertTrue(allGenres.contains("death metal"));
 
     }
 
