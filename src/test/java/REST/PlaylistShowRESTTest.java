6aa07c04c1a73c45c269c10aea0f6a2afe342b57
Dimitris Verdelis <jimverdelis@gmail.com>
2017-03-23 20:16:32 +0200

    playlist show rest test, does not work yet

--- src/test/java/REST/PlaylistShowRESTTest.java
+++ src/test/java/REST/PlaylistShowRESTTest.java
@@ -2,13 +2,19 @@ package REST;
 
 import com.radio.Controllers.MusicTracksLibraryController;
 import com.radio.Controllers.PlaylistShowController;
+import com.radio.Factories.FactoryGenericImpl;
 import com.radio.Initializer;
-import com.radio.Models.MusicTrack;
+import com.radio.Models.*;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by lotv on 23/03/2017.
@@ -23,15 +29,27 @@ public class PlaylistShowRESTTest extends RESTTest{
     protected static MusicTrack musictrack2;
     protected static MusicTrack musictrack4;
     Initializer dataHelper;
+    PlaylistShow playlistShow;
+    FactoryGenericImpl f;
+    Calendar playDateTime;
+    Producer producer;
 
     @Before
     public void setUp(){
-        dataHelper.eraseData();
         musictrack = new MusicTrack();
+        playDateTime = Calendar.getInstance();
+        producer = new Producer("firstName", "lastName", new EmailAddress("testing@email.org"));
+        playlistShow = new PlaylistShow("playListShow",producer,3600, playDateTime.getTimeInMillis());
+
+        f.create(playlistShow);
+
     }
 
     @Test
     public void testAddTrackToPlaylistShow(){
-        String response = target("musicTrackLibrary").request().get(String.class);
+        System.out.println(f.getAll());
+        Response response = target("playListShow/"+playlistShow.getId()).request().post(Entity.json(musictrack));
+        List<Track> tracks = playlistShow.getTracks();
+        Assert.assertEquals(1, tracks.size());
     }
 }
