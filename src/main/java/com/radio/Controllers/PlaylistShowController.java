dbd51f19da610bfee4486ee9a316fdafc5818032
Dimitris Verdelis <jimverdelis@gmail.com>
2017-03-23 18:58:21 +0200

    added playlistshow controller

--- src/main/java/com/radio/Controllers/PlaylistShowController.java
+++ src/main/java/com/radio/Controllers/PlaylistShowController.java
@@ -1,28 +1,56 @@
 package com.radio.Controllers;
 
+import com.radio.Factories.FactoryGenericImpl;
 import com.radio.Models.PlaylistShow;
 import com.radio.Models.Track;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.*;
+import java.net.URI;
+
+@Path("playListShow")
 public class PlaylistShowController extends ShowController{
 
-//    public boolean checkDuration(PlaylistShow playlist){
-//        int sum = 0;
-//        for (Track tr : playlist.getTracks()) {
-//            sum = sum + tr.getDuration();
-//
-//        }
-//
-//        if(sum > playlist.getDuration()){
-//            return true;
-//        }
-//        else{
-//            return false;
-//        }
-//    }
+    @Context
+    UriInfo uriInfo;
 
-//    public PlaylistShow fixDuration(PlaylistShow playlist){
-//        playlist.getTracks().remove(playlist.getTracks().size() - 1);
-//        return playlist;
-//    }
+    FactoryGenericImpl f = new FactoryGenericImpl(PlaylistShow.class);
 
+    @POST
+    @Path("{showId:[0-9]*}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addTrackToPlaylistShow(@PathParam("showId") int showId, Track track) {
+        try {
+            PlaylistShow playlistShow = (PlaylistShow) f.get(showId);
+            playlistShow.addTrackToList(track);
+
+            UriBuilder ub = uriInfo.getAbsolutePathBuilder();
+            URI newPlayListURI = ub.path(Integer.toString(playlistShow.getId())).build();
+
+            return Response.created(newPlayListURI).build();
+
+        } catch (Exception e) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Id " + showId + " not found").build();
+        }
+    }
+
+    @POST
+    @Path("{showId:[0-9]*}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response removeTrackFromPlaylistShow(@PathParam("showId") int showId, Track track) {
+        try{
+            PlaylistShow playlistShow = (PlaylistShow) f.get(showId);
+            playlistShow.removeTrackFromList(track);
+
+            UriBuilder ub = uriInfo.getAbsolutePathBuilder();
+            URI newPlayListURI = ub.path(Integer.toString(playlistShow.getId())).build();
+
+            return Response.created(newPlayListURI).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.NOT_FOUND).entity("Id " + showId + " not found").build();
+        }
+    }
 }
